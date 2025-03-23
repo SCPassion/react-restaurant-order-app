64a879627b3f0f98ee4327920ae2619dbb86214d
SCPassion <141584825+SCPassion@users.noreply.github.com>
2025-03-23 17:26:02 +0100

    create cartfooter jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { menuArray } from './data.js';
-import { nanoid } from 'nanoid'
 import MenuItem from './components/MenuItem.jsx'
+import CartFooter from './components/CartFooter.jsx'
 
 export default function () {
     // State
     const [cart, setCart] = React.useState([])
     const [modelOpen, setModelOpen] = React.useState(false)
     const [paymentDetails, setPaymentDetails] = React.useState(null)
-
-    // Calculations
-    const totalPrice = cart.reduce((total, item) => total + item.price, 0)
 
     // callback functions
     function handleClick(selectedItem) {
@@ -46,24 +43,6 @@ export default function () {
         )
     })
 
-    const cartElements = cart.map(cartItem => {
-        return (
-            <div
-                key={nanoid()}
-                className="flex justify-center w-full gap-4  mb-[22px]"
-            >
-                <p className="text-[28px]">{cartItem.name}</p>
-                <button
-                    className="text-[12px] text-gray-400 font-verdana cursor-pointer hover:text-red-500 hover:text-[16px]"
-                    onClick={() => handleRemoveOrder(cartItem.id)}
-                >
-                    remove
-                </button>
-                <p className="ml-auto text-[20px]">${cartItem.price}</p>
-            </div>
-        )
-    })
-
     return (
         <div className="font-smythe w-[600px] flex flex-col mx-auto mt-4">
             <header className="p-11 bg-[url(./assets/burgerMain.jpeg)] bg-center bg-cover bg-no-repeat text-white">
@@ -76,20 +55,11 @@ export default function () {
             </main>
 
             {(cart.length > 0 && paymentDetails === null) &&
-                <footer className="px-[46px] flex flex-col items-center">
-                    <h2 className="text-[28px] mt-[45px] mb-[64px]">Your order</h2>
-                    {cartElements}
-                    <div className="flex justify-between w-full border-t-2 pt-[31px]">
-                        <h2 className="text-[28px]">Total price: </h2>
-                        <p className="text-[20px]">${totalPrice}</p>
-                    </div>
-
-                    <button
-                        className="bg-[#16DB99] font-verdana text-[16px] font-black text-white w-full py-[18px] my-[53px] cursor-pointer rounded-md hover:bg-green-800 transition-all duration-300 hover:scale-105 hover:text-[24px] hover:shadow-3xl"
-                        onClick={toggleModalState}
-                    >Complete order</button>
-
-                </footer>
+                <CartFooter
+                    cart={cart}
+                    handleRemoveOrder={handleRemoveOrder}
+                    toggleModalState={toggleModalState}
+                />
             }
 
             {paymentDetails !== null &&
---
+++ src/components/CartFooter.jsx
@@ -0,0 +1,42 @@
+import { nanoid } from 'nanoid'
+
+export default function (props) {
+
+    const cartElements = props.cart.map(cartItem => {
+        return (
+            <div
+                key={nanoid()}
+                className="flex justify-center w-full gap-4  mb-[22px]"
+            >
+                <p className="text-[28px]">{cartItem.name}</p>
+                <button
+                    className="text-[12px] text-gray-400 font-verdana cursor-pointer hover:text-red-500 hover:text-[16px]"
+                    onClick={() => props.handleRemoveOrder(cartItem.id)}
+                >
+                    remove
+                </button>
+                <p className="ml-auto text-[20px]">${cartItem.price}</p>
+            </div>
+        )
+    })
+
+    // Calculations
+    const totalPrice = props.cart.reduce((total, item) => total + item.price, 0)
+
+    return (
+        <footer className="px-[46px] flex flex-col items-center">
+            <h2 className="text-[28px] mt-[45px] mb-[64px]">Your order</h2>
+            {cartElements}
+            <div className="flex justify-between w-full border-t-2 pt-[31px]">
+                <h2 className="text-[28px]">Total price: </h2>
+                <p className="text-[20px]">${totalPrice}</p>
+            </div>
+
+            <button
+                className="bg-[#16DB99] font-verdana text-[16px] font-black text-white w-full py-[18px] my-[53px] cursor-pointer rounded-md hover:bg-green-800 transition-all duration-300 hover:scale-105 hover:text-[24px] hover:shadow-3xl"
+                onClick={props.toggleModalState}
+            >Complete order</button>
+
+        </footer>
+    )
+}
